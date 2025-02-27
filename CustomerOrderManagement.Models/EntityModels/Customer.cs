973922776281fd154b6775e9e32a171f949ad00d
Navid Hasin Alvee <NAlvee@rensafamily.company>
2025-02-27 16:06:17 +0600

    made changes

--- CustomerOrderManagement.Models/EntityModels/Customer.cs
+++ CustomerOrderManagement.Models/EntityModels/Customer.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CustomerOrderManagement.Models.EntityModels
 {
@@ -20,8 +19,9 @@ namespace CustomerOrderManagement.Models.EntityModels
 
         [StringLength(150)]
         public string Name { get; set; }
-        
-        public string PhoneNo { get; set; }
+
+        public string PhoneNumber { get; set; }
+
         public string Address { get; set; }
 
         public int CategoryId { get; set; }
