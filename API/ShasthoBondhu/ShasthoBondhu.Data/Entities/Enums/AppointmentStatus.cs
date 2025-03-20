312c16d684b866c3f41f90e97096dc4a371436f8
Shadid Reza Chowdhury <hmshadidreza@gmail.com>
2025-03-21 00:51:19 +0600

    Update AppointmentStatus.cs

--- API/ShasthoBondhu/ShasthoBondhu.Data/Entities/Enums/AppointmentStatus.cs
+++ API/ShasthoBondhu/ShasthoBondhu.Data/Entities/Enums/AppointmentStatus.cs
@@ -3,8 +3,12 @@ namespace ShasthoBondhu.Data.Entities.Enums;
 public enum AppointmentStatus
 {
     Scheduled = 0,
+
     Confirmed = 1,
+
     Completed = 2,
-    Cancelled = 4,
-    NoShow = 8
+
+    Cancelled = 3,
+    
+    NoShow = 4
 }
